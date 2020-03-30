c67ae73aea0f2b3880fb60938ea4232624c96e93
Ashwin Kumar <ashwinkumar9830@gmail.com>
2020-03-30 13:05:59 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+// Step I: The createElement Function
+
+const element = (
+  <div id="foo">
+    <a>bar</a>
+    <b />
+  </div>
+)
+const container = document.getElementById("root")
+ReactDOM.render(element, container)
+
+
+const element = React.createElement(
+  "div",
+  { id: "foo" },
+  React.createElement("a", null, "bar"),
+  React.createElement("b")
+)
+const container = document.getElementById("root")
+ReactDOM.render(element, container)
