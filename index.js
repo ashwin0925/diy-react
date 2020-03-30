e5c93873713e1bd567ff64e87dbfa011cc3ec1a1
Ashwin Kumar <ashwinkumar9830@gmail.com>
2020-03-30 13:29:43 +0530

    step I completed

--- index.js
+++ index.js
@@ -18,3 +18,58 @@ const element = React.createElement(
 )
 const container = document.getElementById("root")
 ReactDOM.render(element, container)
+
+
+function createElement(type, props, ...children) { // spread operator for the props
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map(child =>
+        typeof child === "object"
+          ? child
+          : createTextElement(child)
+      ),
+    },
+  }
+}
+
+function createTextElement(text) {
+  return {
+    type: "TEXT_ELEMENT",
+    props: {
+      nodeValue: text,
+      children: [],
+    },
+  }
+}
+
+const Didact = {
+  createElement,
+}
+
+const element = Didact.createElement(     //replace react element 
+  "div",
+  { id: "foo" },
+  Didact.createElement("a", null, "bar"),
+  Didact.createElement("b")
+)
+const container = document.getElementById("root")
+ReactDOM.render(element, container)
+
+
+
+const Didact = {
+  createElement,
+}
+/** @jsx Didact.createElement */
+const element = (
+  <div id="foo">
+    <a>bar</a>
+    <b />
+  </div>
+)
+const container = document.getElementById("root")
+ReactDOM.render(element, container)
+
+// Step II: The render Function
