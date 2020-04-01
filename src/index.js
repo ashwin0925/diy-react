16859db50c54122c483e5847e28a5be5592c9f6a
Ashwin Kumar <ashwinkumar9830@gmail.com>
2020-04-01 13:02:22 +0530

    Step VI started

--- src/index.js
+++ src/index.js
@@ -41,6 +41,19 @@ function createDom(fiber) {
 
 function commitRoot() {
   // TODO add nodes to dom
+  commitWork(wipRoot.child)
+  currentRoot = wipRoot
+  wipRoot = null
+}
+
+function commitWork(fiber) {
+  if (!fiber) {
+    return
+  }
+  const domParent = fiber.parent.dom
+  domParent.appendChild(fiber.dom)
+  commitWork(fiber.child)
+  commitWork(fiber.sibling)
 }
 
 function render(element, container) {
@@ -49,11 +62,13 @@ function render(element, container) {
     props: {
       children: [element],
     },
+    alternate: currentRoot,
   }
   nextUnitOfWork = wipRoot
 }
 
 let nextUnitOfWork = null
+let currentRoot = null
 let wipRoot = null
 
 function workLoop(deadline) {
@@ -78,34 +93,49 @@ function performUnitOfWork(fiber) {
     fiber.dom = createDom(fiber)
   }
 
-  // if (fiber.parent) {
-  //   fiber.parent.dom.appendChild(fiber.dom)
-  // }
-
-  // TODO create new fibers
   const elements = fiber.props.children
+  reconcileChildren(fiber, elements)
+
+  if (fiber.child) {
+    return fiber.child
+  }
+  let nextFiber = fiber
+  while (nextFiber) {
+    if (nextFiber.sibling) {
+      return nextFiber.sibling
+    }
+    nextFiber = nextFiber.parent
+  }
+}
+
+function reconcileChildren(wipFiber, elements) {
   let index = 0
+  let oldFiber =
+    wipFiber.alternate && wipFiber.alternate.child
   let prevSibling = null
 
-  while (index < elements.length) {
+  while (
+    index < elements.length ||
+    oldFiber != null
+  ) {
     const element = elements[index]
+    let newFiber = null
 
-    const newFiber = {
-      type: element.type,
-      props: element.props,
-      parent: fiber,
-      dom: null,
+    // TODO compare oldFiber to element
+
+    if (oldFiber) {
+      oldFiber = oldFiber.sibling
+      if (index === 0) {
+        wipFiber.child = newFiber
+      } else {
+        prevSibling.sibling = newFiber
+      }
+
+      prevSibling = newFiber
+      index++
     }
-
-    if (index === 0) {
-      fiber.child = newFiber
-    } else {
-      prevSibling.sibling = newFiber
-    }
-
-    prevSibling = newFiber
-    index++
   }
+
 
   // TODO return next unit of work
   if (fiber.child) {
